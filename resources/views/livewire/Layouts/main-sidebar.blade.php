c9b4e38424cf5b4e38fd8fd1eb551d06218529a6
eric <ericgcanubas@gmail.com>
2024-06-09 01:47:31 +0800

    fix sidemenu

--- resources/views/livewire/Layouts/main-sidebar.blade.php
+++ resources/views/livewire/Layouts/main-sidebar.blade.php
@@ -31,11 +31,13 @@
 
                         <ul class="nav nav-treeview">
                             @can('patient.schedule.view')
-                                <li class="nav-item"> <a href="{{ route('patientsschedules') }}"
+                                <li class="nav-item">
+                                    <a href="{{ route('patientsschedules') }}"
                                         class="nav-link {{ request()->is('patients/schedules*') ? 'active' : '' }}"> <i
                                             class="fas fa-calendar nav-icon"></i>
                                         <p>Schedules</p>
-                                    </a> </li>
+                                    </a>
+                                </li>
                             @endcan
                             @can('patient.service-charges.view')
                                 <li class="nav-item"> <a href="{{ route('patientsservice_charges') }}"
@@ -91,14 +93,12 @@
                         Auth::user()->can('customer.received-payment.view') ||
                         Auth::user()->can('customer.statement'))
                     <li class="nav-item {{ request()->is('customers*') ? 'menu-open' : '' }}">
-
                         <a href="#" class="nav-link {{ request()->is('customers*') ? 'active ' : '' }}"> <i
                                 class="nav-icon fas fa-users "></i>
                             <p> Customers <i class="fas fa-angle-left right"></i> </p>
                         </a>
 
                         <ul class="nav nav-treeview">
-
                             @can('customer.sales-order.view')
                                 <li class="nav-item">
                                     <a href="{{ route('customerssales_order') }}"
@@ -157,10 +157,10 @@
                     </li>
                 @endif
 
-                @if (Auth::user()->can('permission:vendor.purchase-order.view') ||
-                        Auth::user()->can('permission:vendor.bill.view') ||
-                        Auth::user()->can('permission:vendor.bill-credit.view') ||
-                        Auth::user()->can('permission:vendor.bill-payment.view'))
+                @if (Auth::user()->can('vendor.purchase-order.view') ||
+                        Auth::user()->can('vendor.bill.view') ||
+                        Auth::user()->can('vendor.bill-credit.view') ||
+                        Auth::user()->can('vendor.bill-payment.view'))
                     <li class="nav-item {{ request()->is('vendors*') ? 'menu-open' : '' }}">
                         <a href="#" class="nav-link {{ request()->is('vendors*') ? 'active ' : '' }}">
                             <i class="nav-icon fas fa-user-tie"></i>
@@ -168,7 +168,7 @@
                         </a>
                         <ul class="nav nav-treeview">
 
-                            @can('permission:vendor.purchase-order.view')
+                            @can('vendor.purchase-order.view')
                                 <li class="nav-item">
                                     <a href="{{ route('vendorspurchase_order') }}"
                                         class="nav-link {{ request()->is('vendors/purchase-order*') ? 'active' : '' }}">
@@ -177,7 +177,7 @@
                                     </a>
                                 </li>
                             @endcan
-                            @can('permission:vendor.bill.view')
+                            @can('vendor.bill.view')
                                 <li class="nav-item">
                                     <a href="{{ route('vendorsbills') }}"
                                         class="nav-link {{ request()->is('vendors/bills*') ? 'active' : '' }}">
@@ -186,7 +186,7 @@
                                     </a>
                                 </li>
                             @endcan
-                            @can('permission:vendor.bill-payment.view')
+                            @can('vendor.bill-payment.view')
                                 <li class="nav-item">
                                     <a href="{{ route('vendorsbill_payment') }}"
                                         class="nav-link {{ request()->is('vendors/bill-payments*') ? 'active' : '' }}">
@@ -195,7 +195,7 @@
                                     </a>
                                 </li>
                             @endcan
-                            @can('permission:vendor.bill-credit.view')
+                            @can('vendor.bill-credit.view')
                                 <li class="nav-item">
                                     <a href="{{ route('vendorsbill_credit') }}"
                                         class="nav-link {{ request()->is('vendors/bill-credits*') ? 'active' : '' }}">
@@ -215,46 +215,52 @@
                 @endif
 
 
-                @if (Auth::user()->can('permission:company.stock-transfer.view') ||
-                        Auth::user()->can('permission:company.build-assembly.view') ||
-                        Auth::user()->can('permission:company.inventory-adjustment.view') ||
-                        Auth::user()->can('permission:general-journal.view'))
+                @if (Auth::user()->can('company.stock-transfer.view') ||
+                        Auth::user()->can('company.build-assembly.view') ||
+                        Auth::user()->can('company.inventory-adjustment.view') ||
+                        Auth::user()->can('general-journal.view'))
                     <li class="nav-item {{ request()->is('company*') ? 'menu-open' : '' }}">
                         <a href="#" class="nav-link {{ request()->is('company*') ? 'active ' : '' }}">
                             <i class="nav-icon fa fa-building"></i>
                             <p> Company <i class="fas fa-angle-left right"></i> </p>
                         </a>
                         <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="{{ route('companystock_transfer') }}"
-                                    class="nav-link {{ request()->is('company/stock-transfer*') ? 'active' : '' }}">
-                                    <i class="fa fa-truck nav-icon"></i>
-                                    <p>Stock Transfer</p>
-                                </a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a href="{{ route('companyinventory_adjustment') }}"
-                                    class="nav-link {{ request()->is('company/inventory-adjustment*') ? 'active' : '' }}">
-                                    <i class="fa fa-adjust nav-icon"></i>
-                                    <p>Inventory Adjustment</p>
-                                </a>
-                            </li>
-
-                            <li class="nav-item">
-                                <a href="{{ route('companybuild_assembly') }}"
-                                    class="nav-link {{ request()->is('company/build-assembly*') ? 'active' : '' }}">
-                                    <i class="fa fa-cube nav-icon"></i>
-                                    <p>Build Assembly</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('companygeneral_journal') }}"
-                                    class="nav-link  {{ request()->is('company/general-journal*') ? 'active' : '' }}">
-                                    <i class="fa fa-table nav-icon"></i>
-                                    <p>General Journal</p>
-                                </a>
-                            </li>
+                            @can('company.stock-transfer.view')
+                                <li class="nav-item">
+                                    <a href="{{ route('companystock_transfer') }}"
+                                        class="nav-link {{ request()->is('company/stock-transfer*') ? 'active' : '' }}">
+                                        <i class="fa fa-truck nav-icon"></i>
+                                        <p>Stock Transfer</p>
+                                    </a>
+                                </li>
+                            @endcan
+                            @can('company.inventory-adjustment.view')
+                                <li class="nav-item">
+                                    <a href="{{ route('companyinventory_adjustment') }}"
+                                        class="nav-link {{ request()->is('company/inventory-adjustment*') ? 'active' : '' }}">
+                                        <i class="fa fa-adjust nav-icon"></i>
+                                        <p>Inventory Adjustment</p>
+                                    </a>
+                                </li>
+                            @endcan
+                            @can('company.build-assembly.view')
+                                <li class="nav-item">
+                                    <a href="{{ route('companybuild_assembly') }}"
+                                        class="nav-link {{ request()->is('company/build-assembly*') ? 'active' : '' }}">
+                                        <i class="fa fa-cube nav-icon"></i>
+                                        <p>Build Assembly</p>
+                                    </a>
+                                </li>
+                            @endcan
+                            @can('general-journal.view')
+                                <li class="nav-item">
+                                    <a href="{{ route('companygeneral_journal') }}"
+                                        class="nav-link  {{ request()->is('company/general-journal*') ? 'active' : '' }}">
+                                        <i class="fa fa-table nav-icon"></i>
+                                        <p>General Journal</p>
+                                    </a>
+                                </li>
+                            @endcan
                         </ul>
                     </li>
                 @endif
@@ -262,37 +268,45 @@
 
 
 
-                @if (Auth::user()->can('permission:banking.deposit.view') ||
-                        Auth::user()->can('permission:banking.fund-transfer.view') ||
-                        Auth::user()->can('permission:banking.make-cheque.view'))
+                @if (Auth::user()->can('banking.deposit.view') ||
+                        Auth::user()->can('banking.fund-transfer.view') ||
+                        Auth::user()->can('banking.make-cheque.view'))
                     <li class="nav-item {{ request()->is('banking*') ? 'menu-open' : '' }}">
                         <a href="#" class="nav-link {{ request()->is('banking*') ? 'active ' : '' }}">
                             <i class="nav-icon fa fa-university"></i>
                             <p> Banking <i class="fas fa-angle-left right"></i> </p>
                         </a>
                         <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="{{ route('bankingdeposit') }}"
-                                    class="nav-link  {{ request()->is('banking/deposit*') ? 'active' : '' }}">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Deposit</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('bankingfund_transfer') }}"
-                                    class="nav-link  {{ request()->is('banking/fund-transfer*') ? 'active' : '' }}">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Fund Transfer</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('bankingmake_cheque') }}"
-                                    class="nav-link  {{ request()->is('banking/make-cheque*') ? 'active' : '' }}">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Make Cheque</p>
-                                </a>
-                            </li>
 
+                            @can('banking.deposit.view')
+                                <li class="nav-item">
+                                    <a href="{{ route('bankingdeposit') }}"
+                                        class="nav-link  {{ request()->is('banking/deposit*') ? 'active' : '' }}">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Deposit</p>
+                                    </a>
+                                </li>
+                            @endcan
+
+                            @can('banking.fund-transfer.view')
+                                <li class="nav-item">
+                                    <a href="{{ route('bankingfund_transfer') }}"
+                                        class="nav-link  {{ request()->is('banking/fund-transfer*') ? 'active' : '' }}">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Fund Transfer</p>
+                                    </a>
+                                </li>
+                            @endcan
+
+                            @can('banking.make-cheque.view')
+                                <li class="nav-item">
+                                    <a href="{{ route('bankingmake_cheque') }}"
+                                        class="nav-link  {{ request()->is('banking/make-cheque*') ? 'active' : '' }}">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Make Cheque</p>
+                                    </a>
+                                </li>
+                            @endcan
                         </ul>
                     </li>
                 @endif
