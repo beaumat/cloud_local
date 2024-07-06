93bbf0c2fef61050b1fd085b6c6b2ab3d2f439d1
eric <eric@coredev.ph>
2024-07-06 16:54:23 +0800

    fx

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('sql:treatment')->dailyAt('16:54');
+        $schedule->command('sql:treatment')->dailyAt('16:55');
     }
 
     /**
