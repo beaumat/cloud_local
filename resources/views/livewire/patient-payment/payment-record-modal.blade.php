11701d4477dec10fbb7b5d508004618e1c0f5f5c
eric <eric@coredev.ph>
2024-07-30 00:18:12 +0800

    done

--- resources/views/livewire/patient-payment/payment-record-modal.blade.php
+++ resources/views/livewire/patient-payment/payment-record-modal.blade.php
@@ -2,15 +2,16 @@
     @if ($showModal)
         <div class="modal show" id="modal-xl" tabindex="-1" role="dialog"
             style="display: block; background-color: rgba(0, 0, 0, 0.5); position: fixed; top: 0; left: 0; width: 100%; height: 100%; overflow: hidden;">
-            <div class="modal-dialog modal-xl" role="document" style="max-width: 100%; height: 100%; margin: 0;">
-                <div class="modal-content" style="height: 100%; display: flex; flex-direction: column;">
+            <div class="modal-dialog modal-xl" role="document"
+                style="width: 90%; max-width: none; height: auto; margin: auto; top: 50%; transform: translateY(-50%);">
+                <div class="modal-content">
                     <div class="modal-header">
                         <h6 class="modal-title">Assistance Record</h6>
                         <button type="button" class="close" wire:click="closeModal">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
-                    <div class="modal-body" style="flex: 1; overflow-y: auto;">
+                    <div class="modal-body">
                         @livewire('Patient.AssistanceRecord', ['CONTACT_ID' => $CONTACT_ID])
                     </div>
                     <div class="modal-footer">
