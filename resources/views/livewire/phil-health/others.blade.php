c3ec0f2273455d1264c83bfbd9396432fce218bd
eric <eric@coredev.ph>
2024-08-26 13:37:09 +0800

    fix RR

--- resources/views/livewire/phil-health/others.blade.php
+++ resources/views/livewire/phil-health/others.blade.php
@@ -7,7 +7,7 @@
             session('message'), 'error' => session('error')])
             <label class="text-xs">RR No.</label>
             <input type="text" class="form-control text-xs w-25"  wire:model='RR_NO' title="RR :"
-                placeholder="Please enter @livewire('layouts.reports-payables')" maxlength="3" />
+                placeholder="Please enter RR" maxlength="3" />
         </div>
         <div class="form-group">
             <button type="submit" class="btn btn-success btn-xs"> <i class="fa fa-floppy-o" aria-hidden="true"></i> Update
