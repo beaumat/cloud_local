fb17318e1443f93e8c69acf544a1916ebf53130d
eric <eric@coredev.ph>
2024-09-21 02:58:34 +0800

    done direct

--- app/Livewire/DashboardPage/Dashboard.php
+++ app/Livewire/DashboardPage/Dashboard.php
@@ -2,15 +2,24 @@
 
 namespace App\Livewire\DashboardPage;
 
+use Illuminate\Support\Facades\Redirect;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title('Dashboard')]
 class Dashboard extends Component
 {
+
+    public function mount()
+    {
+        if (auth()->user()->can('direct-to-treatment')) {
+            if (auth()->user()->can('patient.treatment.view')) {
+                return Redirect::route('patientshemo');
+            }
+        }
+    }
     public function render()
     {
         return view('livewire.dashboard-page.dashboard');
     }
 }
- 
