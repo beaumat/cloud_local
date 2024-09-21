cb231793429d8ec406dd850da528ca7a02c1b247
eric <eric@coredev.ph>
2024-09-21 11:57:23 +0800

    FIX

--- resources/views/livewire/hemodialysis/nurse-notes.blade.php
+++ resources/views/livewire/hemodialysis/nurse-notes.blade.php
@@ -220,7 +220,7 @@
                                                                 <div class="col-12 mb-2">
                                                                     <div class="row">
                                                                         <div class="col-md-6 col-6">
-                                                                            <input type="number" name="AP"
+                                                                        <input type="number" name="AP"
                                                                                 class="text-xs form-control form-control-sm"
                                                                                 placeholder="AP" wire:model='AP' />
                                                                         </div>
