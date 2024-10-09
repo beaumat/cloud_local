2950a01744de1fc9b729cfd7bbc8bcb6b7d77db9
eric <ericcanubas@coredev.ph>
2024-10-10 01:52:59 +0800

    tax

--- resources/views/livewire/tax/tax-form.blade.php
+++ resources/views/livewire/tax/tax-form.blade.php
@@ -31,26 +31,26 @@
                                     <div class="row">
 
                                         <div class="col-md-4">
-                                            <livewire:text-input name="NAME" titleName="Name" wire:model='NAME' />
+                                            <livewire:text-input name="NAME" titleName="Name" wire:model='NAME' :isDisabled='false' />
                                         </div>
                                         <div class="col-md-4" @if ($ID > 0) style="opacity: 0.5;pointer-events: none;" @endif>
-                                            <livewire:select-option name="TAX_TYPE" :options="$taxTypes" :zero="false"
+                                            <livewire:select-option name="TAX_TYPE" :options="$taxTypes" :zero="false" :isDisabled='false'
                                                 titleName="Tax Type" wire:model.live='TAX_TYPE' :key="$taxTypes->pluck('ID')->join('_')" />
                                         </div>
                                         <div class="col-md-4">
-                                            <livewire:number-input name="RATE" titleName="Rate" wire:model='RATE' />
+                                            <livewire:number-input name="RATE" titleName="Rate" wire:model='RATE'  :isDisabled='false' />
                                         </div>
 
                                         @if ($TAX_TYPE == 0)
                                             <div class="col-md-4">
                                                 <livewire:select-option name="TaxAcctID" :options="$accountList"
-                                                    :zero="true" titleName="Tax Account"
+                                                    :zero="true" titleName="Tax Account" :isDisabled='false'
                                                     wire:model.live='TAX_ACCOUNT_ID' />
                                             </div>
                                         @elseif($TAX_TYPE == 1)
                                             <div class="col-md-4">
                                                 <livewire:select-option name="TaxAcctID1" :options="$accountList"
-                                                    :zero="true" titleName="Tax Account"
+                                                    :zero="true" titleName="Tax Account" :isDisabled='false'
                                                     wire:model.live='TAX_ACCOUNT_ID' />
                                             </div>
                                         @elseif($TAX_TYPE == 2)
@@ -61,24 +61,24 @@
                                             </div>
                                             <div class="col-md-4">
                                                 <livewire:select-option name="AssetAcctID" :options="$accountList2"
-                                                    :zero="true" titleName="Tax Credit Account"
+                                                    :zero="true" titleName="Tax Credit Account" :isDisabled='false'
                                                     wire:model.live='ASSET_ACCOUNT_ID' />
                                             </div>
                                         @elseif($TAX_TYPE == 3)
                                             <div class="col-md-4">
                                                 <livewire:select-option name="VAT_METHOD" :options="$vatMethod"
-                                                    :zero="false" titleName="Vat Method"
+                                                    :zero="false" titleName="Vat Method" :isDisabled='false'
                                                     wire:model.live='VAT_METHOD' :key="$vatMethod->pluck('ID')->join('_')" />
                                             </div>
                                             <div class="col-md-4">
                                                 <livewire:select-option name="TaxAcctID" :options="$accountList"
-                                                    :zero="true" titleName="Output Tax Account"
+                                                    :zero="true" titleName="Output Tax Account" :isDisabled='false'
                                                     wire:model.live='TAX_ACCOUNT_ID' />
                                             </div>
 
                                             <div class="col-md-4">
                                                 <livewire:select-option name="AssetAcctID" :options="$accountList2"
-                                                    :zero="true" titleName="Input Tax Account"
+                                                    :zero="true" titleName="Input Tax Account" :isDisabled='false'
                                                     wire:model.live='ASSET_ACCOUNT_ID' />
                                             </div>
 
@@ -86,7 +86,7 @@
                                         @elseif($TAX_TYPE == 4)
                                             <div class="col-md-4">
                                                 <livewire:select-option name="TaxAcctID" :options="$accountList"
-                                                    :zero="true" titleName="Tax Account"
+                                                    :zero="true" titleName="Tax Account" :isDisabled='false'
                                                     wire:model.live='TAX_ACCOUNT_ID' />
                                             </div>
                                         @endif
@@ -94,7 +94,7 @@
 
 
                                         <div class="col-md-12">
-                                            <livewire:custom-check-box name="INACTIVE" titleName="Inactive"
+                                            <livewire:custom-check-box name="INACTIVE" titleName="Inactive" :isDisabled='false'
                                                 wire:model='INACTIVE' />
                                         </div>
                                     </div>
