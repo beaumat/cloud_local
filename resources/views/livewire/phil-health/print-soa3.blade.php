1ae58f30b6d6f13287238db5844464a58d3d0e8e
eric <ericgcanubas@gmail.com>
2024-12-10 07:06:30 +0800

    fix

--- resources/views/livewire/phil-health/print-soa3.blade.php
+++ resources/views/livewire/phil-health/print-soa3.blade.php
@@ -18,15 +18,34 @@
                 </div>
                 <div class="col-12">
                     <div class="row">
-                        <div class="col-12 text-center">
-                            <h5>STATEMENT OF ACCOUNT</h5>
+                        <div class="col-4">
+                            <b class="bottom-line2"
+                                @if ($HEADER) style="opacity: 0.0" @endif>
+                                PHILHEALTH ACCREDITED :
+                                </b>
+                        </div>
+                        <div class="col-4 text-center mb-1">
+                            <h4>STATEMENT OF ACCOUNT</h4>
+                        </div>
+                        <div class="col-4">
+                            <div class="row">
+                                <div @if ($HEADER) style="opacity: 0.0" @endif
+                                    class="col-5 text-right">
+                                    SOA No. : </div>
+                                <div class="col-5 @if ($OUTPUT_SIGN) bottom-line2 @endif">
+                                    <b>{{ $CODE }}</b>
+                                </div>
+                                <div class="col-2">
+
+                                </div>
+                            </div>
                         </div>
                         <div class="col-6">
-                            <b class="bottom-line"
-                                @if ($HEADER) style="opacity: 0.0" @endif>PHILHEALTH ACCREDITED
-                                :</b>
-                            <div class="row mt-2">
-                                <div class="col-12 text-right">
+                            <div class="row">
+                                <div class="col-6">
+
+                                </div>
+                                <div class="col-6 text-right">
                                     <i>DOB</i>: {{ $DATE_BIRTH }}
                                 </div>
                                 <div class="col-4" @if ($HEADER) style="opacity: 0.0" @endif>
@@ -66,14 +85,9 @@
                         </div>
                         <div class="col-6">
                             <div class="row">
-                                <div @if ($HEADER) style="opacity: 0.0" @endif class="col-5">
-                                    SOA No. : </div>
-                                <div class="col-7 @if ($OUTPUT_SIGN) bottom-line @endif">
-                                    <b>{{ $CODE }}</b>
+                                <div class="col-12">
+
                                 </div>
-                            </div>
-                            <div class="row mt-2">
-                                <div class="col-12"> &nbsp;</div>
                                 <div @if ($HEADER) style="opacity: 0.0" @endif class="col-5">
                                     PHILHEALTH No. :
                                 </div>
@@ -683,7 +697,8 @@
                                     @if (!$OUTPUT_SIGN) style="opacity: 0.0" @endif>
                                     <b>{{ $PATIENT_NAME }}</b>
                                 </div>
-                                <div class="col-12 " @if (!$OUTPUT_SIGN) style="opacity: 0.0" @endif>
+                                <div class="col-12 text-sm"
+                                    @if (!$OUTPUT_SIGN) style="opacity: 0.0" @endif>
                                     Member/Patient/Authorized Representative</div>
                                 <div class="col-12 " @if (!$OUTPUT_SIGN) style="opacity: 0.0" @endif>
                                     (Signature over printed name)</div>
@@ -702,7 +717,8 @@
                                     </span>
 
                                 </div>
-                                <div class="col-12 text-xs" @if (!$OUTPUT_SIGN) style="opacity: 0.0" @endif>
+                                <div class="col-12 text-xs"
+                                    @if (!$OUTPUT_SIGN) style="opacity: 0.0" @endif>
                                     CONTACT No. {{ $PATIENT_CONTACT }}</div>
                             </div>
                         </div>
