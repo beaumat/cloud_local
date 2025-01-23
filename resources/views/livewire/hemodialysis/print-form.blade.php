2918e8c634eb200a8b1c017f139f095f9fab2b1b
eric <ericgcanubas@gmail.com>
2025-01-23 15:27:34 +0800

    s1

--- resources/views/livewire/hemodialysis/print-form.blade.php
+++ resources/views/livewire/hemodialysis/print-form.blade.php
@@ -13,13 +13,7 @@
 
 @script
     <script>
-        $wire.on('print', () => {
-            var printContents = document.getElementById('printableContent').innerHTML;
-            var originalContents = document.body.innerHTML;
-            document.body.innerHTML = printContents;
-            window.print();
-            document.body.innerHTML = originalContents;
-        });
+  
 
         function printPageAndClose() {
             window.print();
