3e1591b97f4f1f88d190f11c6e7b3ce024b66cf8
eric <ericgcanubas@gmail.com>
2025-03-07 09:49:23 +0800

    sasa

--- app/Livewire/Patient/MedcertModal.php
+++ app/Livewire/Patient/MedcertModal.php
@@ -66,40 +66,70 @@ class MedcertModal extends Component
     {
         $this->showModal = false;
     }
-    public function SaveChange()
+    // public function SaveChange()
+    // {
+
+    //     $this->validate(
+    //         [
+
+    //             'MED_CERT_NURSE_ID' => 'required|exists:contact,id',
+
+    //         ],
+    //         [],
+    //         [
+    //             'MED_CERT_NURSE_ID' => 'Duty Physician'
+    //         ]
+    //     );
+
+    //     try {
+    //         $this->medCertServices->UpdatePatientMedCert(
+    //             $this->PATIENT_ID,
+    //             $this->MED_CERT_NURSE_ID,
+    //             $this->FIX_MON,
+    //             $this->FIX_TUE,
+    //             $this->FIX_WEN,
+    //             $this->FIX_THU,
+    //             $this->FIX_FRI,
+    //             $this->FIX_SAT,
+    //             $this->FIX_SUN
+    //         );
+    //         session()->flash('message', 'Successfully updated');
+    //     } catch (\Exception $th) {
+    //         session()->flash('error', $th->getMessage());
+    //     }
+    // }
+    public function updatedFixMon()
     {
-
-        $this->validate(
-            [
-
-                'MED_CERT_NURSE_ID' => 'required|exists:contact,id',
-
-            ],
-            [],
-            [
-                'MED_CERT_NURSE_ID' => 'Duty Physician'
-            ]
-        );
-
-        try {
-            $this->medCertServices->UpdatePatientMedCert(
-                $this->PATIENT_ID,
-                $this->MED_CERT_NURSE_ID,
-                $this->FIX_MON,
-                $this->FIX_TUE,
-                $this->FIX_WEN,
-                $this->FIX_THU,
-                $this->FIX_FRI,
-                $this->FIX_SAT,
-                $this->FIX_SUN
-            );
-            session()->flash('message', 'Successfully updated');
-        } catch (\Exception $th) {
-            session()->flash('error', $th->getMessage());
-        }
+        $this->medCertServices->updateParamater($this->PATIENT_ID, ['FIX_MON' => $this->FIX_MON]);
     }
-
-
+    public function updatedFixTue()
+    {
+        $this->medCertServices->updateParamater($this->PATIENT_ID, ['FIX_TUE' => $this->FIX_TUE]);
+    }
+    public function updatedFixWen()
+    {
+        $this->medCertServices->updateParamater($this->PATIENT_ID, ['FIX_WEN' => $this->FIX_WEN]);
+    }
+    public function updatedFixThu()
+    {
+        $this->medCertServices->updateParamater($this->PATIENT_ID, ['FIX_THU' => $this->FIX_THU]);
+    }
+    public function updatedFixFri()
+    {
+        $this->medCertServices->updateParamater($this->PATIENT_ID, ['FIX_FRI' => $this->FIX_FRI]);
+    }
+    public function updatedFixSat()
+    {
+        $this->medCertServices->updateParamater($this->PATIENT_ID, ['FIX_SAT' => $this->FIX_SAT]);
+    }
+    public function updatedFixSun()
+    {
+        $this->medCertServices->updateParamater($this->PATIENT_ID, ['FIX_SUN' => $this->FIX_SUN]);
+    }
+    public function updatedMEDCERTNURSEID()
+    {
+        $this->medCertServices->updateParamater($this->PATIENT_ID, ['MED_CERT_NURSE_ID' => $this->MED_CERT_NURSE_ID]);
+    }
     public function render()
     {
 
