708f5a083d46acbacba9b6787f00a3a4d02d61f1
eric <ericgcanubas@gmail.com>
2025-03-17 16:26:37 +0800

    asasa

--- resources/views/livewire/hemodialysis/agreement-form-page2.blade.php
+++ resources/views/livewire/hemodialysis/agreement-form-page2.blade.php
@@ -90,19 +90,15 @@
                         $limit--;
                         $total = $total + ($list->QUANTITY * $list->RATE ?? 0);
                     @endphp
-                    <div class="col-6 top-line2 right-line2 "> &nbsp;
-                        @if ($list->DESCRIPTION)
-                            {{ $list->DESCRIPTION }}
-                        @else
-                            <strong class="text-danger font-weight-bold">N/A</strong>
-                        @endif
-                    </div>
+                    <div class="col-6 top-line2 right-line2 "> &nbsp; {{ $list->DESCRIPTION }} </div>
                     <div class="col-3 top-line2 right-line2 text-center"> &nbsp; {{ $list->QUANTITY }} </div>
                     <div class="col-3 top-line2 text-center "> &nbsp; {{ number_format($list->RATE, 2) }} </div>
                 @endforeach
 
                 @for ($n = 1; $n <= $limit; $n++)
-                    <div class="col-6 top-line2 right-line2 "> &nbsp;          <strong class="text-danger font-weight-bold">N/A</strong> </div>
+                    <div class="col-6 top-line2 right-line2 "> &nbsp;
+                        <strong class="text-danger font-weight-bold">N/A</strong>
+                    </div>
                     <div class="col-3 top-line2 right-line2 "> &nbsp; </div>
                     <div class="col-3 top-line2  "> &nbsp; </div>
                 @endfor
