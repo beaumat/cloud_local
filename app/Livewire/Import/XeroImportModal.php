c2eacef02396a3d5c20f943b98e0cd9b0ca1d4ef
eric <ericgcanubas@gmail.com>
2025-04-16 08:00:52 +0800

    asa

---
+++ app/Livewire/Import/XeroImportModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Import;
+
+use Livewire\Component;
+
+class XeroImportModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.import.xero-import-modal');
+    }
+}
