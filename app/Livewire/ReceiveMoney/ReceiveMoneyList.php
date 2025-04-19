82d195626acae16ec34e7ba6ba0a81ced206fa70
eric <ericgcanubas@gmail.com>
2025-04-20 01:50:49 +0800

    asas

---
+++ app/Livewire/ReceiveMoney/ReceiveMoneyList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\ReceiveMoney;
+
+use Livewire\Component;
+
+class ReceiveMoneyList extends Component
+{
+    public function render()
+    {
+        return view('livewire.receive-money.receive-money-list');
+    }
+}
---
+++ app/Livewire/SpendMoney/SendMoneyList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\SpendMoney;
+
+use Livewire\Component;
+
+class SendMoneyList extends Component
+{
+    public function render()
+    {
+        return view('livewire.spend-money.send-money-list');
+    }
+}
