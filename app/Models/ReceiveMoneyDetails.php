e5c46230856b47925fdf8603f4ce5d15804ba9fe
eric <ericgcanubas@gmail.com>
2025-04-22 07:31:22 +0800

    assa

--- app/Models/ReceiveMoneyDetails.php
+++ app/Models/ReceiveMoneyDetails.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class ReceiveMoneyDetails extends Model
 {
     use HasFactory;
+    protected $table = 'receive_money_details';
+    protected $primaryKey = 'ID';
+    public $timestamps = false;
+    protected $fillable = [
+        'ID',
+        'RECEIVE_MONEY_ID',
+        'LINE_NO',
+        'ACCOUNT_ID',
+        'AMOUNT',
+        'NOTES'
+    ];
 }
