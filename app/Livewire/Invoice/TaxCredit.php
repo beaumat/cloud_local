ecdb9eaba9946dc46ed04626eaabae1b53781cb2
eric <ericgcanubas@gmail.com>
2025-04-27 17:44:01 +0800

    asas

--- app/Livewire/Invoice/TaxCredit.php
+++ app/Livewire/Invoice/TaxCredit.php
@@ -78,7 +78,7 @@ class TaxCredit extends Component
         if ($tax) {
             $this->EWT_RATE  = $tax->RATE ?? 0;
             $this->AMOUNT_WITHHELD = $this->AMOUNT * ($this->EWT_RATE / 100);
-            $this->EWT_ACCOUNT_ID = $tax->TAX_ACCOUNT_ID ?? 0;
+            $this->EWT_ACCOUNT_ID = $tax->ASSET_ACCOUNT_ID ?? 0;
             $acctData =   $this->accountServices->Get($this->EWT_ACCOUNT_ID);
             if ($acctData) {
                 $this->TAX_DESCRIPTION = $acctData->NAME  ?? '';
