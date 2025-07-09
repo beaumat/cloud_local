099c8acbd79f80233ed1cf2492b749367935ae5f
EGC <ericgcanubas@gmail.com>
2025-07-09 17:46:09 +0800

    ds1

--- resources/views/livewire/patient-report/philhealth-annex-two.blade.php
+++ resources/views/livewire/patient-report/philhealth-annex-two.blade.php
@@ -47,7 +47,6 @@
                                     </div>
                                     <div class='col-2'>
 
-
                                     </div>
                                 </div>
                             </div>
