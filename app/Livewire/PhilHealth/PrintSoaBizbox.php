a3dd0ef19f503c787cdbdf821681fbeed7dac0e8
Your Name <ericgcanubas@gmail.com>
2025-07-23 08:16:50 +0800

    ds1

---
+++ app/Livewire/PhilHealth/PrintSoaBizbox.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\PhilHealth;
+
+use Livewire\Component;
+
+class PrintSoaBizbox extends Component
+{
+    public function render()
+    {
+        return view('livewire.phil-health.print-soa-bizbox');
+    }
+}
