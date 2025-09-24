3847cf3493028d7bdbf206c49660c65f5a6391f0
Your Name <ericgcanubas@gmail.com>
2025-09-24 23:18:44 +0800

    asdas

--- app/Services/AccountJournalEndingServices.php
+++ app/Services/AccountJournalEndingServices.php
@@ -17,16 +17,22 @@ class AccountJournalEndingServices
 
     public function ResetFirstEntryAccount(int $ACCOUNT_ID, int $LOCATION_ID)
     {
-        $result =  AccountJournal::where('ACCOUNT_ID','=', $ACCOUNT_ID)
-        ->where('LOCATION_ID','=', $LOCATION_ID)
-        ->orderBy('OBJECT_DATE','asc')
-        ->orderBy('ID','asc')
-        ->first();
+        $result = AccountJournal::where('ACCOUNT_ID', '=', $ACCOUNT_ID)
+            ->where('LOCATION_ID', '=', $LOCATION_ID)
+            ->orderBy('OBJECT_DATE', 'asc')
+            ->orderBy('ID', 'asc')
+            ->first();
 
-        if($result) {
+        if ($result) {
+            $this->SetZeroBalance($result->ID);
             $this->Recount($result->ID);
         }
 
+    }
+    public function SetZeroBalance(int $JOURNAL_ACCOUNT_ID)
+    {
+        AccountJournal::where('ID', '=', $JOURNAL_ACCOUNT_ID)
+            ->update(['ENDING_BALANCE' => '0.00']);
     }
 
 }
