c62acf71f9c702dd0553b965813a02f481532da1
Eric Canubas <ericgcanubas@gmail.com>
2025-09-26 10:52:31 +0800

    asdas'

--- app/Services/TimerServices.php
+++ app/Services/TimerServices.php
@@ -95,8 +95,8 @@ class TimerServices
     }
     public function getExecute()
     {
-       $transDate = $this->dateServices->NowDate();
-       // $transDate = $this->dateServices->BackDate();
+       //$transDate = $this->dateServices->NowDate();
+        $transDate = $this->dateServices->BackDate();
 
         $this->generateUnposted();
         $this->generateWaitingList($transDate);
