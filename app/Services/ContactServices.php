4dfb511fe2d3b85ae37f98c1bee028fe6e717ce7
Eric G. Canubas <ericgcanubas@gmail.com>
2025-12-17 08:08:43 +0800

    asda

--- app/Services/ContactServices.php
+++ app/Services/ContactServices.php
@@ -771,8 +771,8 @@ class ContactServices
                 });
             })
             ->when($locationId > 0, function ($query) use (&$locationId, &$search, &$doctorId) {
-                $query->where('contact.LOCATION_ID', '=', $locationId);
-
+                $query->where('contact.LOCATION_ID', '=', $locationId)
+                 ->where('dl.LOCATION_ID','=', $locationId);
 
             })
             ->orderBy($sortBy, $isDesc ? 'desc' : 'asc')
